9081f5bbaf65110f90db4c6ab63df53c2e9c7a09
Abhiraj R <113856741+4bhy@users.noreply.github.com>
2023-04-06 02:00:30 +0530

    apiii

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 module.exports={
-    REACT_APP_BE_URL:'https://lounge-backend.onrender.com/api',
+    REACT_APP_BE_URL:'https://lounge-backend.onrender.com',
     REACT_APP_STRIPE_PUBLIC_KEY:'pk_test_51MgPNUSGJWduBmwsipwpGdodNDToVW3dynb2wJ1WodeBiNCTmOpTzuTD9350zWhEH4p4mRPCNnomI1b7uFCGJzt70016bD7AeD'
 
 }
